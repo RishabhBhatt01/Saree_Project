3c78dd794bb497567393fd3bfa67ab433eacb4df
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-18 20:54:00 +0530

    Refactor database connection code for consistency and readability

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,13 +1,12 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 async function connectDB() {
-  try{
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log("Connected to the database")
-  }catch{
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to the database");
+  } catch {
     console.log("Unable to connect to the database");
   }
 }
-
-module.exports = connectDB
+module.exports = connectDB;
