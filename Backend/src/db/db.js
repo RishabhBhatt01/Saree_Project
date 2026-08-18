893bbc27693f09fb0c9bcc535ce4d4b8774a3ac6
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-18 17:20:54 +0530

    Add database connection utility using mongoose

---
+++ Backend/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+async function connectDB() {
+  try{
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connected to the database")
+  }catch{
+    console.log("Unable to connect to the database");
+  }
+}
+
+module.exports = connectDB
