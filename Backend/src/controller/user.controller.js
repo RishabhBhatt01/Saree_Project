483e85a6b8daf834c0498f7f0d19185d857bf13e
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-19 11:49:39 +0530

    Add user controller to handle user retrieval by ID

---
+++ Backend/src/controller/user.controller.js
@@ -0,0 +1,11 @@
+const userModel = require("../models/user.model");
+const userController = async (req, res) => {
+  try {
+    const id = req.userId;
+    const user = await userModel.findOne({ _id: id });
+    res.status(200).json({ message: "success", user });
+  } catch {
+    res.status(404).json({ message: "User not found" });
+  }
+};
+module.exports=userController
