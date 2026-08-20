698b37ac6b254b89f935e5980bddada61f2a24e8
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-20 07:15:06 +0530

    Add role field to user schema with enum validation

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -5,7 +5,12 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
   username : String,
   email : String,
-  password : String
+  password : String,
+  role : {
+    type : String,
+    enum : ["user","admin"],
+    default : "user"
+  }
 })
 
 const User = mongoose.model("user", userSchema)
