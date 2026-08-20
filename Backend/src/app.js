21eb687f3841aae55b6f3f97a1d13b97be90760a
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-20 08:18:43 +0530

    Add admin routes for fetching admin-related data

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 
-
-
 // Route Imports
 const authRoutes = require("../src/routes/auth.routes")
 const userRoutes = require("../src/routes/user.routes")
+const adminRoutes = require("../src/routes/admin.routes")
 
 
 const app = express()
@@ -17,6 +16,8 @@ app.use(cookieParser())
 // Routes
 app.use("/api/auth",authRoutes)
 app.use("/api/fetch",userRoutes)
+app.use("/api/fetch",adminRoutes)
+
 
 
 module.exports = app
