90d4f99555ebd8bf60f302c9c6deb6b03b8e5979
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-24 23:51:33 +0530

    Add saree routes for fetching sarees and a specific saree by ID

--- Backend/src/routes/saree.routes.js
+++ Backend/src/routes/saree.routes.js
@@ -3,14 +3,26 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const authMiddleware = require("../middleware/auth.middleware");
 const adminMiddleware = require("../middleware/admin.middleware");
-const sareeController = require("../controller/saree.controller");
+const {sareeController,getSarees, getOneSaree} = require("../controller/saree.controller");
 
 const router = express.Router();
 router.post(
-  "/sarees",
+  "/create-sarees",
   authMiddleware,
   adminMiddleware,
   upload.single("sareeImg"),
   sareeController,
 );
+
+router.get(
+  "/getsarees",
+  getSarees
+)
+
+
+router.get(
+  "/getOneSaree/:id",
+  getOneSaree
+)
+
 module.exports = router;
