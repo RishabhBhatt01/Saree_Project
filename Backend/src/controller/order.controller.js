995252f640b0ba4ab666dab03197181cb9f9dbe6
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-29 14:03:36 +0530

    Refactor orderController to improve error handling and ensure cart retrieval is awaited

--- Backend/src/controller/order.controller.js
+++ Backend/src/controller/order.controller.js
@@ -1,25 +1,75 @@
 const cartModel = require("../models/cart.model");
-const sareeModel = require("../models/saree.model")
+const sareeModel = require("../models/saree.model");
+const orderModel = require("../models/orders.model");
 const orderController = async (req, res) => {
-  // Logged-in user
-  const userId = req.userId;
-  // Find their Cart
-  const cart = cartModel.findOne({userId})
+  try {
+    console.log("in controller");
 
-  // Is cart empty?
-  if(!cart || cart.items.length == 0){
+    // Logged-in user
+    const userId = req.userId;
+    // Find their Cart
+    const cart = await cartModel.findOne({ userId });
+
+    // Is cart empty?
+    if (!cart || cart.items.length == 0) {
+      res.status(400).json({
+        message: "Empty or no cart",
+      });
+      return;
+    }
+
+    const orderItems = [];
+    let totalAmount = 0;
+    // Get saree prices
+    for (const item of cart.items) {
+      const saree = await sareeModel.findById(item.sareeId);
+      if (!saree) {
+        res.status(400).json({ message: "can't find the saree" });
+        return;
+      }
+      orderItems.push({
+        sareeId: item.sareeId,
+        quantity: item.quantity,
+        price: saree.price,
+      });
+    }
+    // Calculate totalAmount
+    for (const item of orderItems) {
+      totalAmount += item.price * item.quantity;
+    }
+
+    // Create Order
+    const order = await orderModel.create({
+      userId,
+      items: orderItems,
+      totalAmount,
+    });
+
+    // Clear Cart
+    cart.items = [];
+    await cart.save();
+    // Send response
+    res.status(201).json({
+      message: "Order Placed",
+      order,
+    });
+  } catch (error) {
+    console.log(error);
     res.status(400).json({
-      message : "Empty or no cart"
-    })
-    return
+      error: "error in order controller",
+      order,
+    });
   }
-
-  // Get saree prices
-  const saree = sareeModel.findOne({})
-
-  // Create Order
-  // Calculate totalAmount
-  // Save Order
-  // Clear Cart
-  // Send response
 };
+
+const getOrderController = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const order = await orderModel.findOne({ userId });
+    res.status(200).json({ message: "order fetched successfully", order });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "error in getOrderController" });
+  }
+};
+module.exports = {orderController,getOrderController};
