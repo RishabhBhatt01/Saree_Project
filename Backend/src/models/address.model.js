e56d7e245ac09a4707888a92c9890b2758bc69c5
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-29 21:35:52 +0530

    Add address model for storing user addresses

---
+++ Backend/src/models/address.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const addressModel = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  adreessLine: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: String,
+    required: true,
+  }   
+});
+
+const Address = new mongoose.model("address", addressModel);
+module.exports = Address;
