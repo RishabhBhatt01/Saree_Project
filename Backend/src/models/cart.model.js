e9a8ea33024746624215cf39f785eb10900b9213
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-29 14:04:37 +0530

    Add cart model with schema for user and items

---
+++ Backend/src/models/cart.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+
+  items: [
+    {
+      sareeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "saree",
+        required: true,
+      },
+
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+    },
+  ],
+});
+
+const Cart = mongoose.model("cart", cartSchema);
+
+module.exports = Cart;
