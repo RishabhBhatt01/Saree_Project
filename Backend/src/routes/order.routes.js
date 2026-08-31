e2470f9d90997fe4c586f406ae740ff771553410
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-08-31 06:40:16 +0530

    Refactor order routes to improve code readability by destructuring controller imports

--- Backend/src/routes/order.routes.js
+++ Backend/src/routes/order.routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const authMiddleware = require("../middleware/auth.middleware");
-const {orderController,getOrderController,getOneOrderController} = require("../controller/order.controller");
+const {
+  orderController,
+  getOrderController,
+  getOneOrderController,
+} = require("../controller/order.controller");
 
 const router = express.Router();
-console.log("here");
 
 router.post("/create-order", authMiddleware, orderController);
 router.get("/get-order", authMiddleware, getOrderController);
 router.get("/get-one-order/:orderId", authMiddleware, getOneOrderController);
-
-
-console.log("in routes page");
 
 module.exports = router;
