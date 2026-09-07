43e33921f053c24944867c824308f760911c8516
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-09-07 19:19:07 +0530

    Fix indentation and formatting in saree model schema

--- Backend/src/models/saree.model.js
+++ Backend/src/models/saree.model.js
@@ -4,10 +4,13 @@ const sareeSchema = new mongoose.Schema({
   name: String,
   sareeImg: String,
   price: Number,
+  stock : Number,
   fabric: String,
   category: String,
   description: String,
-});
+},{timestamps : true});
 
+
+sareeSchema.index({stock : 1});
 const Saree = mongoose.model("saree", sareeSchema);
 module.exports = Saree;
