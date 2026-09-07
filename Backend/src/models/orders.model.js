f190f94efc7e8aa5b9c9f662e3e3bc7ce98f255b
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-09-07 19:18:58 +0530

    Refactor order schema for improved readability and structure

--- Backend/src/models/orders.model.js
+++ Backend/src/models/orders.model.js
@@ -1,61 +1,69 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-
-  items: [
-    {
-      sareeId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "saree",
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-        min: 1,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-    },
-  ],
-  totalAmount: {
-    type: Number,
-    required: true,
-  },
-  shippingAddress: {
-    fullName: {
-      type: String,
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
       required: true,
     },
-    phone: {
+    items: [
+      {
+        sareeId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "saree",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
       type: Number,
       required: true,
     },
-    addressLine: {
-      type: String,
-      required: true,
+    shippingAddress: {
+      fullName: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: Number,
+        required: true,
+      },
+      addressLine: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      pincode: {
+        type: Number,
+        required: true,
+      },
     },
-    city: {
+    status: {
       type: String,
-      required: true,
-    },
-    state: {
-      type: String,
-      required: true,
-    },
-    pincode: {
-      type: Number,
-      required: true,
+      enum: ["Pending", "Confirmed", "Delivered", "Shipped"],
+      default: "Pending",
     },
   },
-});
+  { timestamps: true },
+);
 
-const Order = new mongoose.model("order", orderSchema);
+orderSchema.index({ createdAt: -1 });
+const Order = mongoose.model("order", orderSchema);
 module.exports = Order;
