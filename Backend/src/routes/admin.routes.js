7d6f8103650023adc3ffb6e8f7b255305544ed00
Rishabh Bhatt <145821236+RishabhBhatt01@users.noreply.github.com>
2026-09-07 19:19:17 +0530

    Fix formatting and add newlines in admin routes for consistency

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -1,9 +1,14 @@
-const express = require("express")
-const adminController = require("../controller/admin.controller")
-const adminMiddleware = require("../middleware/admin.middleware")
-const authMiddleware = require("../middleware/auth.middleware")
+const express = require("express");
+const adminController = require("../controller/admin.controller");
+const adminMiddleware = require("../middleware/admin.middleware");
+const authMiddleware = require("../middleware/auth.middleware");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/admin-dashboard",authMiddleware ,adminMiddleware,adminController)
-module.exports=router
+router.get(
+  "/admin-dashboard",
+  authMiddleware,
+  adminMiddleware,
+  adminController,
+);
+module.exports = router;
